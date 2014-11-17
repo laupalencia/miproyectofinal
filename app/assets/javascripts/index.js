f9b88e0005d00d60dc0b858282d92b8c37d414a8
unknown <laupalencia@gmail.com>
2014-11-17 18:23:24 +0100

    tercer commit

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,40 @@
+var createEventAtMoment =function(moment, title) {
+	var event = {
+		"title" : title,
+		"allDay": false,
+		"start": moment,
+		"end": moment.time('01:00:00'),
+		"editable": true
+	}
+	$('#calendar').fullCalendar('renderEvent', event);
+}
+
+//Aquí, al hacer click en el día, te crea un evento
+var dayClicked = function(date, jsEvent, view) {
+	//displayEventCreationOptions()
+	createEventAtMoment(date, 'Evento de prueba');
+}
+
+var createEventWithOptions = function() {
+	//extraer datos para el evento de las opciones del overlay
+	//createEventAtMoment(...)
+}
+
+var displayEventCreationOptions = function() {
+	//mostrar overlay con opciones para crear entrada en calendario
+}
+
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+    	dayClick: dayClicked
+        //header: {
+		    //left:   'title',
+		    //center: '',
+		    //right:  'today prev,next'
+		//}
+    })
+
+});
