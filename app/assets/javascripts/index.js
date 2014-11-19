7d6d45313e174101aebfe3fc1cd6174c1b64b3d7
unknown <laupalencia@gmail.com>
2014-11-19 18:40:36 +0100

    miercoles 19 de nov

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,34 +1,34 @@
 var createEventAtMoment =function(moment, title) {
-	var event = {
-		"title" : title,
-		"allDay": false,
-		"start": moment,
-		"end": moment.time('01:00:00'),
-		"editable": true
-	}
-	$('#calendar').fullCalendar('renderEvent', event);
+  var event = {
+    "title" : title,
+    "allDay": false,
+    "start": moment,
+    "end": moment.time('01:00:00'),
+    "editable": true
+  }
+  $('#calendar').fullCalendar('renderEvent', event);
 }
 
 //Hacer click en un día
 var dayClicked = function(date, jsEvent, view) {
-	//displayEventCreationOptions()
+  //displayEventCreationOptions()
 
-	//Con esto de abajo se crea un nuevo evento donde pinchas:
-	//createEventAtMoment(date, 'Evento de prueba');
-	
-	//Con esto al hacer click en un día, se muestra el formulario
-	$('#myModal').modal('show') 
+  //Con esto de abajo se crea un nuevo evento donde pinchas:
+  //createEventAtMoment(date, 'Evento de prueba');
+  
+  //Con esto al hacer click en un día, se muestra el formulario
+  $('#myModal').modal('show') 
 }
 
 
 
 var createEventWithOptions = function() {
-	//extraer datos para el evento de las opciones del overlay
-	//createEventAtMoment(...)
+  //extraer datos para el evento de las opciones del overlay
+  //createEventAtMoment(...)
 }
 
 var displayEventCreationOptions = function() {
-	//mostrar overlay con opciones para crear entrada en calendario
+  //mostrar overlay con opciones para crear entrada en calendario
 }
 
 $(document).ready(function() {
@@ -36,20 +36,22 @@ $(document).ready(function() {
     // page is now ready, initialize the calendar...
 
     $('#calendar').fullCalendar({
-    	dayClick: dayClicked
+      dayClick: dayClicked
         //header: {
-		    //left:   'title',
-		    //center: '',
-		    //right:  'today prev,next'
-		//}
+        //left:   'title',
+        //center: '',
+        //right:  'today prev,next'
+    //}
     })
 
 });
 
 $(document).ready(function()
-	{
-		//Aquí abajo llamas al id del boton de Guardar, que cuando hagas click
-	$("#save").click(function () {
-	createEventAtMoment(date, '#comida'.val());
-	});
+  {
+    //Aquí abajo llamas al id del boton de Guardar, que cuando hagas click
+  $("#save").click(function (event, data) {
+    console.log(event);
+  createEventAtMoment($('#comida').val());
+  });
 });
+
