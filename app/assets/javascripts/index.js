64612c0d460a45e34a635461f209cfbed9ffa1b6
unknown <laupalencia@gmail.com>
2014-11-19 20:10:04 +0100

    miercoles última hora

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,48 +1,60 @@
-var createEventAtMoment =function(moment, title) {
+var initCalendar = function() {
+  $('#calendar').fullCalendar({
+    dayClick: dayClicked
+      //header: {
+      //left:   'title',
+      //center: '',
+      //right:  'today prev,next'
+  //}
+  });
+};
+
+var createEventAtMoment =function(title) {
   var event = {
     "title" : title,
     "allDay": false,
-    "start": moment,
-    "end": moment.time('01:00:00'),
+    "start": date,
+    "end": date,
     "editable": true
   }
   $('#calendar').fullCalendar('renderEvent', event);
-}
+};
 
 //Hacer click en un día
 var dayClicked = function(date, jsEvent, view) {
   //displayEventCreationOptions()
-
   //Con esto de abajo se crea un nuevo evento donde pinchas:
   //createEventAtMoment(date, 'Evento de prueba');
-  
-  //Con esto al hacer click en un día, se muestra el formulario
-  $('#myModal').modal('show') 
-}
+  $('#myModal').modal('show');
+  $('#myModal').on('show', function(title) {
+    alert(date);
+    var event = {
+      "title" : "title",
+      "allDay": false,
+      "start": date,
+      "end": date,
+      "editable": true
+    }
+    $('#calendar').fullCalendar('renderEvent', event);
+  });
+};
 
 
 
 var createEventWithOptions = function() {
   //extraer datos para el evento de las opciones del overlay
   //createEventAtMoment(...)
-}
+};
 
 var displayEventCreationOptions = function() {
   //mostrar overlay con opciones para crear entrada en calendario
-}
+};
 
 $(document).ready(function() {
 
     // page is now ready, initialize the calendar...
-
-    $('#calendar').fullCalendar({
-      dayClick: dayClicked
-        //header: {
-        //left:   'title',
-        //center: '',
-        //right:  'today prev,next'
-    //}
-    })
+    initCalendar();
+    
 
 });
 
@@ -51,7 +63,8 @@ $(document).ready(function()
     //Aquí abajo llamas al id del boton de Guardar, que cuando hagas click
   $("#save").click(function (event, data) {
     console.log(event);
-  createEventAtMoment($('#comida').val());
+    var title = $('#comida').val();
+    createEventAtMoment(title);
   });
 });
 
