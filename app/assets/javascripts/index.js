4bba69a14fac436d041f756911c0ece4cdb6fcc9
unknown <laupalencia@gmail.com>
2014-11-24 11:25:40 +0100

    crear eventos con modal en full calendar

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,25 +1,25 @@
 //Aquí se inicia el calendario
 var initCalendar = function() {
-  $('#calendar').fullCalendar({
-    dayClick: dayClicked
-      //header: {
-      //left:   'title',
-      //center: '',
-      //right:  'today prev,next'
-  //}
+  $("#calendar").fullCalendar({
+    dayClick: dayClicked,
+    eventClick: eventClicked,
+    timeFormat: ''  //con esto se oculta la hora en el calendario
   });
 };
 
+var eventClicked = function(calEvent, jsEvent, view) {
+  //alert('Event: ' + calEvent.title);
+  //alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+  //alert('View: ' + view.name);
+
+  //Estoy cambiando esto:
+  document.calendarEvent.title = $("#").val();
+
+}
+
 //Aquí se crea el evento
-var createEventAtMoment =function(title) {
-  var event = {
-    "title" : title,
-    "allDay": false,
-    "start": date,
-    "end": date,
-    "editable": true
-  }
-  $('#calendar').fullCalendar('renderEvent', event);
+var createEvent =function(event) {
+  $("#calendar").fullCalendar("renderEvent", event);
 };
 
 //Clickar en un día
@@ -27,29 +27,14 @@ var dayClicked = function(date, jsEvent, view) {
   //displayEventCreationOptions()
 
   //Con esto de abajo se crea un nuevo evento donde pinchas:
-  //createEventAtMoment(date, 'Evento de prueba');
-  $('#myModal').modal('show');
-  $('#myModal').on('show', function(title) {
-    alert(date);
-    var event = {
-      "title" : "title",
+  //createEventAtMoment(date, "Evento de prueba");
+  $("#myModal").modal("show");
+  document.calendarEvent = {
       "allDay": false,
       "start": date,
       "end": date,
       "editable": true
-    }
-    $('#calendar').fullCalendar('renderEvent', event);
-  });
-};
-
-
-var createEventWithOptions = function() {
-  //extraer datos para el evento de las opciones del overlay
-  //createEventAtMoment(...)
-};
-
-var displayEventCreationOptions = function() {
-  //mostrar overlay con opciones para crear entrada en calendario
+  };
 };
 
 $(document).ready(function() {
@@ -57,27 +42,52 @@ $(document).ready(function() {
     initCalendar();
 });
 
+var resetAndHideModal = function() {
+  $("#comida").val("");
+  $("#myModal").modal("hide");
+}
+
 $(document).ready(function()
   {
     //Aquí abajo llamas al id del boton de Guardar, que cuando hagas click
-  $("#save").click(function (event, data) {
-    console.log(event);
-    var title = $('#comida').val();
-    createEventAtMoment(title);
+  $("#save").click(function (event) {
+    document.calendarEvent.title = $("#comida").val();
+    var moment = document.calendarEvent.start;
+
+    var meal = $("#meal").val();
+
+    if (meal == 0)
+    {
+      document.calendarEvent.start = moment.time("09:00:00");
+      document.calendarEvent.start = moment.time("10:00:00");
+      document.calendarEvent.backgroundColor = "#8EBBCA";
+    }
+    else if (meal == 1)
+    {
+      document.calendarEvent.start = moment.time("13:00:00");
+      document.calendarEvent.start = moment.time("14:00:00");
+      document.calendarEvent.backgroundColor = "#6193A4";
+    }
+    else {
+      document.calendarEvent.start = moment.time("21:00:00");
+      document.calendarEvent.start = moment.time("22:00:00");
+      document.calendarEvent.backgroundColor = "#477584";
+    }
+
+    createEvent(document.calendarEvent);
+    resetAndHideModal();
   });
 });
-
-
 
 ////////////////////
 //Lista de la compra
 ////////////////////
-var mydiv = document.getElementById('addinput');
+var mydiv = document.getElementById("addinput");
 mydiv.onclick = function(){
    //Variables que nos recoge el valor de los inputs
-  var producto = document.getElementById('producto').value;
-  var precio = document.getElementById('precio').value;
-  var lugar = document.getElementById('lugar').value;
+  var producto = document.getElementById("producto").value;
+  var precio = document.getElementById("precio").value;
+  var lugar = document.getElementById("lugar").value;
 
   var h1 = document.createElement("h3");
   var h2 = document.createElement("h4");
@@ -88,8 +98,8 @@ mydiv.onclick = function(){
   var textoPrecio = document.createTextNode(precio);
   var textoLugar = document.createTextNode(lugar);
   //
-  var myli = document.createElement('li');
-  var mylist = document.getElementById('list');
+  var myli = document.createElement("li");
+  var mylist = document.getElementById("list");
   //
   h1.appendChild(textoProducto);
   h2.appendChild(textoPrecio);
@@ -106,7 +116,7 @@ mydiv.onclick = function(){
 //Calculo global
 ////////////////////
 
-var mycalc = document.getElementById('addcalc');
+var mycalc = document.getElementById("addcalc");
 mycalc.onclick = function(){
-  document.getElementById('calculoglobal').style.display = 'block';
+  document.getElementById("calculoglobal").style.display = "block";
   }
